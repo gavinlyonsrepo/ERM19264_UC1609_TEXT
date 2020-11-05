5bb67684e08e765e1ec34f27bd0c19fa30bc6b78
Gavin lyons <glyons66@hotmail.com>
2020-11-05 00:18:50 +0000

    change reset delay times

--- src/ERM19264_UC1609_T.h
+++ src/ERM19264_UC1609_T.h
@@ -70,8 +70,8 @@
 #define UC1609_ROTATION_FLIP_THREE 0x00
 
 // Delays
-#define UC1609_RESET_DELAY 150 // mS delay
-#define UC1609_RESET_DELAY2   1 // mS delay
+#define UC1609_RESET_DELAY 50 // mS delay
+#define UC1609_RESET_DELAY2   0 // mS delay
 #define UC1609_INIT_DELAY 100   //  mS delay
 #define UC1609_INIT_DELAY2 3 // mS delay
 
